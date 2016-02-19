34ed7a6e26e2afb6043b4ec32ae9e26a99f760d9
John-Titor-2036 <mkurisu4@gmail.com>
2016-02-19 14:56:12 -0600

    Added custom URL recognition

--- Form-submit.js
+++ Form-submit.js
@@ -4,19 +4,28 @@ function photoIncluded(){
 	var addedPhotos = document.getElementsByTagName("img");
 	for(var i = 0; i < addedPhotos.length; i++){
 		var addedPhoto = addedPhotos[i];
-		if(addedPhoto.alt === FBPhrase)
-			return true;
+		if(addedPhoto.alt === FBPhrase){
+			return addedPhoto.getAttribute("src");
+		}
 	}
-	return false;
+	return null;
 }
 
-$("textarea").on('click', function(){	
-	if(photoIncluded()){
-		run("http://static01.nyt.com/images/2013/03/03/magazine/03wmt1/03wmt1-articleLarge-v2.jpg", fillTextBox);
-	}
+$("textarea").on('click', function(){
+	var result = photoIncluded();
+	if(result != null)
+		run(result, fillTextBox);
 });
 
 function fillTextBox(){
+	
+	for(var i = 0; i < tags.length; i++){
+		tag = tags[i];
+		if(tag.indexOf(" ") >= 0){
+			array.splice(i, 1);
+		}
+	}
+	
 	if(tags.length > 7){
 		for(var i = 0; i < 7; i++){
 			var tag = tags[i];
@@ -28,7 +37,6 @@ function fillTextBox(){
 			$("textarea").sendkeys("#" + tag + " ");
 		}
 	}
-
-
+	
 }
 console.log("Hooo");
