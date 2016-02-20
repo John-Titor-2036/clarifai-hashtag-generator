70fb2414785e8a35de1157e6214d6e175ba6c723
John-Titor-2036 <mkurisu4@gmail.com>
2016-02-20 00:11:47 -0600

    Twitter autocomplete finished

--- Form-submit.js
+++ Form-submit.js
@@ -1,75 +1,110 @@
 // Twitter
 // Jerry Zhou and Samarth Madduru
-/*
-function photoIncluded(){
-	var addedPhotos = document.getElementsByTagName("img");
-	for(var i = 0; i < addedPhotos.length; i++){
-		var addedPhoto = addedPhotos[i];
-		if(addedPhoto.alt === FBPhrase){
-			return addedPhoto.getAttribute("src");
-		}
-	}
-	return null;
-}
-*/
+var site = document.location.host;
 
-$("#tweet-box-home-timeline").on('click', function(){
+function photoIncluded() {
+    var addedPhotos = document.getElementsByTagName("img");
+    for (var i = 0; i < addedPhotos.length; i++) {
+        var addedPhoto = addedPhotos[i];
+        if (site === "twitter.com") {
+            if (addedPhoto.parentElement.parentElement.parentElement.parentElement.className === "thumbnail-container") {
+                var img = new Image();
+                img.src = addedPhoto.getAttribute("src");
+                var imgHeight = img.height;
+                var imgWidth = img.width;
+				
+                var canvas = document.createElement("canvas")
+                canvas.width = imgHeight;
+                canvas.height = imgWidth;
+                var context = canvas.getContext("2d")
+                context.drawImage(img, 0, 0)
+
+                var filesOnPage = document.getElementsByTagName("span");
+                for (var i = 0; i < filesOnPage.length; i++) {
+                    var file = filesOnPage[i];
+					var extension = file.innerHTML;
+                    extension = extension.substring(extension.lastIndexOf('.'));
+                    // Only process image files.
+                    if(extension === ".jpg" && file.className === "filename"){
+						var data = canvas.toDataURL("images/jpeg");
+						data = data.substring(data.indexOf(",")+1);
+						return data;
+					}else if(extension === ".png" && file.className === "filename"){
+						var data = canvas.toDataURL("images/png");
+						data = data.substring(data.indexOf(",")+1);
+						return data;
+					}
+                }
+
+            }
+        } else 
+			
+		if (site === "facebook.com") {
+            if (addedPhoto.alt === FBPhrase) {
+                return addedPhoto.getAttribute("src");
+            }
+        }
+    }
+    return null;
+}
+
+function fillTextBox(tags) {
+    var hashTags = tags;
+
+    for (var i = 0; i < hashTags.length; i++) {
+        tag = hashTags[i];
+        if (tag.indexOf(" ") >= 0) {
+            hashTags.splice(i, 1);
+        }
+    }
+
+    if (hashTags.length > 7) {
+        if (site === "facebook.com") {
+            for (var i = 0; i < 7; i++) {
+                var tag = tags[i];
+                $("textarea").sendkeys("#" + tag + " ");
+            }
+        } else if (site === "twitter.com") {
+            for (var i = 0; i < 7; i++) {
+                var tag = hashTags[i];
+                console.log(tag);
+                $("#tweet-box-home-timeline").sendkeys("#" + tag + " ");
+            }
+        }
+    } else {
+        if (site === "facebook.com") {
+            for (var i = 0; i < hashTags.length; i++) {
+                var tag = tags[i];
+                $("textarea").sendkeys("#" + tag + " ");
+            }
+        } else if (site === "twitter.com") {
+            for (var i = 0; i < hashTags.length; i++) {
+                var tag = hashTags[i];
+                console.log(tag);
+                $("#tweet-box-home-timeline").sendkeys("#" + tag + " ");
+            }
+        }
+    }
+
+}
+
+$("#tweet-box-home-timeline").on('click', function() {
+
+    // Validate current website
+    if (document.location.host == "twitter.com") {
+        var result = photoIncluded();
+        if (result != null)
+            run(result, fillTextBox);
+    }
+
+});
+
+$("textarea").on('click', function() {
 	
 	// Validate current website
-	if(document.location.host == "twitter.com"){
-		$("#tweet-box-home-timeline").sent("ASDFASDFASDFASDFASDFASDAFDFSA");
-	}
-	/*
-	var result = photoIncluded();
-	if(result != null)
-		run(result, fillTextBox);
-	*/
+    if (site = "facebook.com") {
+        var result = photoIncluded();
+        if (result != null)
+            run(result, fillTextBox);
+    }
 });
-
-
-
-
-
-
-/*
-function photoIncluded(){
-	var addedPhotos = document.getElementsByTagName("img");
-	for(var i = 0; i < addedPhotos.length; i++){
-		var addedPhoto = addedPhotos[i];
-		if(addedPhoto.alt === FBPhrase){
-			return addedPhoto.getAttribute("src");
-		}
-	}
-	return null;
-}
-
-$("textarea").on('click', function(){
-	var result = photoIncluded();
-	if(result != null)
-		run(result, fillTextBox);
-});
-
-function fillTextBox(){
-	
-	for(var i = 0; i < tags.length; i++){
-		tag = tags[i];
-		if(tag.indexOf(" ") >= 0){
-			array.splice(i, 1);
-		}
-	}
-	
-	if(tags.length > 7){
-		for(var i = 0; i < 7; i++){
-			var tag = tags[i];
-			$("textarea").sendkeys("#" + tag + " ");
-		}
-	}else{
-		for(var i = 0; i < tags.length; i++){
-			var tag = tags[i];
-			$("textarea").sendkeys("#" + tag + " ");
-		}
-	}
-	
-}
-console.log("Hooo");
-*/
--- clarifai.js
+++ clarifai.js
@@ -1,61 +1,63 @@
-var tags = [];
 function getCredentials(cb) {
-  var data = {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET
-  };
+    var data = {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    };
 
-  return $.ajax({
-    'url': 'https://api.clarifai.com/v1/token',
-    'data': data,
-    'type': 'POST'
-  })
-  .then(function(r) {
-    localStorage.setItem('accessToken', r.access_token);
-    localStorage.setItem('tokenTimestamp', Math.floor(Date.now() / 1000));
-    cb();
-  });
+    return $.ajax({
+            'url': 'https://api.clarifai.com/v1/token',
+            'data': data,
+            'type': 'POST'
+        })
+        .then(function(r) {
+            localStorage.setItem('accessToken', r.access_token);
+            localStorage.setItem('tokenTimestamp', Math.floor(Date.now() / 1000));
+            cb();
+        });
 }
 
 function postImage(imgurl, callback) {
-  var data = {
-    'url': imgurl
-  };
-  var accessToken = localStorage.getItem('accessToken');
+    if (imgurl.indexOf("http") >= 0 && imgurl.indexOf("http") <= 10) {
+        var data = {
+            'url': imgurl
+        }
+    } else {
+        var data = {
+            'encoded_data': imgurl
+        };
+    }
+    var accessToken = localStorage.getItem('accessToken');
 
-  return $.ajax({
-    'url': 'https://api.clarifai.com/v1/tag',
-    'headers': {
-      'Authorization': 'Bearer ' + accessToken
-    },
-    'data': data,
-    'type': 'POST'
-  }).then(function(r){
-    parseResponse(r, callback);
-  });
+    return $.ajax({
+        'url': 'https://api.clarifai.com/v1/tag',
+        'headers': {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        'data': data,
+        'type': 'POST'
+    }).then(function(r) {
+        parseResponse(r, callback);
+    });
 }
 
 function parseResponse(resp, callback) {
-  if (resp.status_code === 'OK') {
-    var results = resp.results;
-    tags = results[0].result.tag.classes;
-  } else {
-    console.log('Sorry, something is wrong.');
-  }
-	console.log(tags);
-	callback();
-  //$('#tags').text(tags.toString().replace(/,/g, ', '));
-  return tags;
+    var tags;
+    if (resp.status_code === 'OK') {
+        var results = resp.results;
+        var tags = results[0].result.tag.classes;
+        callback(tags);
+    } else {
+        console.log('Sorry, something is wrong.');
+    }
 }
 
 function run(imgurl, callback) {
-  if (localStorage.getItem('tokenTimeStamp') - Math.floor(Date.now() / 1000) > 86400
-    || localStorage.getItem('accessToken') === null) {
-    getCredentials(function() {
-      postImage(imgurl, callback);
-    });
-  } else {
-    postImage(imgurl, callback);
-  }
+    if (localStorage.getItem('tokenTimeStamp') - Math.floor(Date.now() / 1000) > 86400 || localStorage.getItem('accessToken') === null) {
+        getCredentials(function() {
+            postImage(imgurl, callback);
+        });
+    } else {
+        postImage(imgurl, callback);
+    }
 }
